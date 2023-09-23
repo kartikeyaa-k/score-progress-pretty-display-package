61bc382e2979e14d794814b1a0f6eb45a38e0cf6
kartikeya <kartikeya.199231@gmail.com>
2023-09-23 23:27:04 +0530

    add licence

--- lib/score_progress_pretty_display.dart
+++ lib/score_progress_pretty_display.dart
@@ -1,6 +1,6 @@
-library score_progress_pretty_display;
-
 /// lib folder contains of src folder where files ending with components are primary widgets.
 /// arc_progress_painter_types folder consists of custom painters.
+
+library score_progress_pretty_display;
 
 export 'src/primary_arc_animation_component.dart';
